7ecccb6605cec941a0f500107067b597c3ee9733
clcardenas <claudio.cardenas@mercadolibre.com>
2022-05-24 18:00:03 -0300

    Create ParkingSpace class v1

--- src/main/kotlin/ParkingSpace.kt
+++ src/main/kotlin/ParkingSpace.kt
@@ -1,10 +1,11 @@
 import java.util.*
 
+
 data class ParkingSpace(var vehicle:Vehicle){
     val plate:String = vehicle.plate
-    val checkInTime:Long=0
-    val discountCard:String=""
+    val checkInTime=vehicle.checkInTime
+    val discountCard=vehicle.discountCard
     val parkedTime:Long
-        get() = (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+        get() = (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / 60000
 
 }

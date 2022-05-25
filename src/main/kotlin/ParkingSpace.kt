bd930d55a3c3c4641ef2ffd7b6e9dacf3348a47b
MLcaro <carolina.lobos@mercadolibre.cl>
2022-05-25 16:15:58 -0400

    Add sample data to main class

--- src/main/kotlin/ParkingSpace.kt
+++ src/main/kotlin/ParkingSpace.kt
@@ -1,11 +1,46 @@
 import java.util.*
 
 
-data class ParkingSpace(var vehicle:Vehicle){
+data class ParkingSpace(var vehicle:Vehicle, val parking:Parking){
     val plate:String = vehicle.plate
     val checkInTime=vehicle.checkInTime
     val discountCard=vehicle.discountCard
-    val parkedTime:Long
-        get() = (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / 60000
+    var parkedTime :Int=0
+
+
+    fun checkOutVehicle( plate : String, type :VehicleType ) {
+        try {
+            val vehicle: Vehicle? = parking.vehicles.find { it.plate ==plate }
+
+            var type : VehicleType = vehicle?.type?:VehicleType.CAR
+            parkedTime= ((Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / 60000).toInt()
+
+            onSuccess(calculateFee(type, parkedTime))
+        }catch (e: ArithmeticException){
+            onError(plate)
+        }
+    }
+
+    fun calculateFee(type: VehicleType, time: Int) :Int{
+        var totalAmount=0
+        var base=type.price
+        var extraTimeParked=time-120
+        if(parkedTime>120){
+            totalAmount=(extraTimeParked/15)*5+base
+        }else{
+            totalAmount= base
+        }
+        return totalAmount
+    }
+
+    fun onSuccess(totalAmount:Int){
+        println("Your total invoice is $$totalAmount")
+    }
+
+    fun onError(plate:String){
+        println("it wasn't possible checkout a car with plate $plate")
+    }
+
+
 
 }
